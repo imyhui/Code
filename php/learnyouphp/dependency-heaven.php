f3a410f9c1bdcd44a169bc9b6cf4f2d67ba5d588
liuyonghui <1359158019@qq.com>
2018-08-19 02:02:56 +0800

    update readme

--- php/learnyouphp/dependency-heaven.php
+++ php/learnyouphp/dependency-heaven.php
@@ -1,8 +1,5 @@
 <?php
 
-date_default_timezone_set("Europe/London");
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
 require_once __DIR__ . '/vendor/autoload.php';
 use Stringy\Stringy as S;
 $klein = new \Klein\Klein();
